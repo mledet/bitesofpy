9cf39477804f7fed4dd257dea284ac678da4abe7
Michael Ledet <mledet@BCI-3071-MLEDET.local>
2020-04-25 19:28:42 -0500

    Initial Solution Byte 48

--- 48/safari.py
+++ 48/safari.py
@@ -1,4 +1,5 @@
 import os
+import re
 import urllib.request
 
 TMP = os.getenv("TMP", "/tmp")
@@ -11,6 +12,40 @@ urllib.request.urlretrieve(
     SAFARI_LOGS
 )
 
+def get_date(line):
+    
+    date = line[0:5]
+
+    return date
+
 
 def create_chart():
-    pass
+
+    previous_date = None
+
+    with open(SAFARI_LOGS,"r") as f:
+        lines = f.read().splitlines()
+
+        for line in lines:
+
+            if "sending to slack channel" in line:
+
+                date = get_date(line)
+
+                if date != previous_date:
+                    if previous_date:
+                        print("")
+                    print(f"{date} ", end="")    
+
+                if "python" in previous_line.lower():
+                    print("🐍", end="")
+                else:
+                    print(".", end="")
+
+                previous_date = date
+            previous_line = line
+                        
+
+                
+
+    
